06041fea2d59b07322718c2351fce92bf95c2212
azka-rafif <azka.rafif@evermos.com>
2023-07-26 09:56:12 +0700

    turned into 1 string

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ func main() {
 	fmt.Println("main")
 
 	wordOne, wordTwo := readWords("hi", "everyone")
+	sentence := turnIntoSentence(wordOne, wordTwo)
 }
 
 func readWords(sOne, sTwo string) (string, string) {
 	fmt.Println(sOne)
 	fmt.Println(sTwo)
 	return sOne, sTwo
+}
+
+func turnIntoSentence(wOne, wTwo string) string {
+	fmt.Printf("%v %v", wOne, wTwo)
+	return fmt.Sprintf("%v %v", wOne, wTwo)
 }
