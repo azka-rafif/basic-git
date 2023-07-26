451323f22c91f542f5df49f37a674268f6f8d5ec
azka-rafif <azka.rafif@evermos.com>
2023-07-26 09:51:19 +0700

    added read words function

--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("main")
+
+	wordOne, wordTwo := readWords("hi", "everyone")
+}
+
+func readWords(sOne, sTwo string) (string, string) {
+	fmt.Println(sOne)
+	fmt.Println(sTwo)
+	return sOne, sTwo
+}
